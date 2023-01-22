b07abf515abd13a88289320c5cfc54cef23a00fa
Firas Dahmani <firas1dahmani@gmail.com>
2023-01-22 19:49:39 +0100

    Firas
    work fine

---
+++ script.js
@@ -0,0 +1,71 @@
+// settings up the Unsplash API
+const apiKey = "888pFXi1v3kQuwx4hATXAK8_9njVx6WgCIigqQgIWbU"
+const count = 10
+const apiUrl = `https://api.unsplash.com/photos/random?count=${count}&client_id=${apiKey}`
+
+let arrayGallery = []
+let ready = false
+let imagesLaded = 0
+let totalImages = 0
+
+const galleryContainer = document.querySelector('#box')
+const err = document.querySelector('#error')
+
+// Check if the images loaded equals the total number of images to display more images
+function imageLaded (){
+    imagesLaded++
+    if(totalImages === imagesLaded){
+        ready = true
+    }
+}
+
+//  set elements attributes for the gallery container
+function setAttribute(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create the gallery container and attach it to the document
+function displayImage() {
+    totalImages = arrayGallery.length
+    imagesLaded = 0
+    arrayGallery.forEach((photo) => {
+        const item = document.createElement('a')
+        setAttribute(item, {
+            'href': photo.links.html,
+            'target': '_blank'
+        })
+
+        const imageElement = document.createElement('img')
+        setAttribute(imageElement, {
+            'alt': photo.alt_description,
+            'title': photo.alt_description,
+            'src': photo.urls.regular
+        })
+        imageLaded()
+        item.appendChild(imageElement)
+        galleryContainer.appendChild(item)
+    })
+}
+
+// get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl)
+        arrayGallery = await response.json()
+        displayImage()
+    } catch (error) {
+        err.textContent = "OOPS! Something went wrong ... "
+    }
+}
+
+getPhotos() // get photos
+
+// check to see if scrolling is near the bottom of the page , load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        getPhotos()
+        ready = false
+    }
+})
